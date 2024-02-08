7501b210b4c34db55d326e97a238c20597036dcb
AdityaMehar294 <aditya.mehar@zehntech.com>
2024-02-08 14:11:40 +0530

    Add main.js which perform Addition and Substraction

---
+++ main.js
@@ -0,0 +1,13 @@
+
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  console.log("Main branch:");
+  console.log("Addition:", add(5, 3));
+  console.log("Subtraction:", subtract(8, 2));
+  
